62bb3e3792dd48ebe0b500403723a729a194efb7
mohamedsaad <mahameds19999@gmail.com>
2020-03-16 23:08:56 +0200

    Init the course page

--- src/mentoor/modules/courses/components/course-details-page/course-details-page.component.js
+++ src/mentoor/modules/courses/components/course-details-page/course-details-page.component.js
@@ -1,21 +1,33 @@
 class CourseDetailsPage {
-    /**
-     * Constructor
-     * Put your required dependencies in the constructor parameters list  
-     */
-    constructor() {
-        this.name = 'course-details';
-        this.title = trans('course-details');
-    }
-    
-    /**
-     * Initialize the component
-     * This method is triggered before rendering the component
-     */
-    init() {}
+  /**
+   * Constructor
+   * Put your required dependencies in the constructor parameters list
+   */
+  constructor(coursesService, router) {
+    this.name = "course-details";
+    this.title = trans("course-details");
 
-    /**
-     * The component is ready to do any action after being rendered in dom
-     */
-    ready() {}
+    this.coursesService = coursesService;
+
+    this.router = router;
+  }
+
+  /**
+   * Initialize the component
+   * This method is triggered before rendering the component
+   */
+  init() {
+    this.isLoading = true;
+
+    let id = this.router.params.id;
+    this.coursesService.get(id).then(response => {
+      this.course = response.record;
+      this.isLoading = false;
+    });
+  }
+
+  /**
+   * The component is ready to do any action after being rendered in dom
+   */
+  ready() {}
 }
